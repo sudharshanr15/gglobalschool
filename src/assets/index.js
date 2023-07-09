78ac894b1d111d5c01feded13ce1d1b0e32dcec5
Sudharshan <sudharshanrms2004@gmail.com>
2023-07-09 17:07:45 +0530

    Added navbar

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import gglobal_logo from "./logo/gglobal.svg"
+
+import icon_close from "./icons/close.svg"
+
+export { 
+    gglobal_logo, 
+    icon_close,
+};
---
+++ src/component/navbar/Navbar.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+import { useRef, useState } from "react"
+
+import { gglobal_logo, icon_close } from "@/assets/index"
+
+import styles from "./styles.module.css"
+
+function Navbar(){
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [isMenuHover, setIsMenuHover] = useState(false)
+
+    function onMenuClick(){
+        setIsMenuOpen((e) => (
+            !e
+        ))
+    }
+
+    function onMenuHover(){
+        setIsMenuHover((e) => (
+            !e
+        ))
+    }
+
+    return (
+        <nav className="bg-transparent relative">
+            <div className="ps-8 py-6 flex flex-row items-center">
+                <div className="flex-1">
+                    <div className="inline-block px-7 pb-4 pt-2 bg-white rounded-3xl">
+                        <Image className="max-w-[12rem]" src={gglobal_logo} unoptimized alt="Gglobal School Logo" />
+                    </div> 
+                </div>
+                <div className="inline-flex gap-x-16 items-center">
+                    <Link href="" className="text-subheading-regular text-white">Admissions</Link>
+                    <button className={`flex flex-row items-center bg-primary-yellow-1 hover:bg-primary-maroon-1 hover:text-white text-gray-900 px-7 py-6 rounded-l-full ${isMenuHover ? styles.open : ''}`}
+                        onClick={onMenuClick}
+                        onMouseEnter={onMenuHover}
+                        onMouseLeave={onMenuHover}
+                    >
+                        <div className={styles.menu_icon}>
+                            <span className={`${styles.line} ${styles.line_1}`}></span>
+                            <span className={`${styles.line} ${styles.line_2}`}></span>
+                            <span className={`${styles.line} ${styles.line_3}`}></span>
+                        </div>
+                        <h6 className="text-heading-6">Menu</h6>
+                    </button>
+                </div>
+            </div>
+            {isMenuOpen && (
+                <div className="w-full bg-primary-maroon-1 absolute top-0 left-0 p-10">
+                    <div className="flex flex-row justify-between items-center">
+                        <div className="inline-block px-7 pb-4 pt-2 bg-white rounded-3xl">
+                            <Image className="max-w-[12rem]" src={gglobal_logo} unoptimized alt="Gglobal School Logo" />
+                        </div>
+                        <button className="inline-flex items-center justify-center bg-primary-orange-1 w-20 aspect-square rounded-full"
+                            onClick={onMenuClick}
+                        >
+                            <Image src={icon_close} unoptimized alt="Close Icon" />
+                        </button>
+                    </div>
+                    <div className="mt-10">
+                        <ul className="list-none">
+                            <li className="text-heading-6 text-white mb-8">
+                                <Link href="">G Global Universe</Link>
+                            </li>
+                            <li className="text-heading-6 text-white mb-8">
+                                <Link href="">Curriculum</Link>
+                            </li>
+                            <li className="text-heading-6 text-white mb-8">
+                                <Link href="">Community Wellness</Link>
+                            </li>
+                            <li className="text-heading-6 text-white mb-8">
+                                <Link href="">Discovery Outside the wall</Link>
+                            </li>
+                            <li className="text-heading-6 text-white mb-8">
+                                <Link href="">Days & Events</Link>
+                            </li>
+                            <li className="text-heading-6 text-white mb-8">
+                                <Link href="">Become one of us</Link>
+                            </li>
+                        </ul>
+                        <button className="mt-8 px-11 py-5 rounded-full bg-primary-yellow-1 text-heading-6 text-gray-900">Contact us</button>
+                    </div>
+                </div>
+            )}
+        </nav>
+    )
+}
+
+export default Navbar
