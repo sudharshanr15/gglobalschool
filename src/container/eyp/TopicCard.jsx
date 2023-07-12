ecabcf969de8a386e7a5d1d8a1a6d0a55251bf80
kishorekumars2609 <kk26092002@gmail.com>
2023-07-12 12:53:42 +0530

    starting with responsiveness

--- src/container/eyp/TopicCard.jsx
+++ src/container/eyp/TopicCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import img from "../../assets/Rectangle 79.png"
+import img from "../../assets/Rectangle 78.png"
 import Image from 'next/image'
 
 function TopicCard({text}) {
   return (
     <>
-      <div className='content-center px-4  ' >
-        <Image src={img} alt="checked" className="w-[100%]" />
-        <div className="bg-primary-maroon-1">
+      <div className='content-center px-10 w-full md:w-[80%] h-[40%]  ' >
+        <Image src={img} alt="checked" className=" md:left-3 w-full md:w-full md:w-auto" />
+        <div className="bg-primary-maroon-1 w-full text-center py-4">
           <p className="text-white uppercase text-heading-5">{ text }</p>
         </div>
       </div>
