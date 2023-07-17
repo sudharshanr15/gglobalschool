12e50b1a0a984ace6659b079e80592c9fccf6516
p-karthikeyan <keyan4676@gmail.com>
2023-07-17 18:07:30 +0530

    corrections made

--- src/container/StudentCouncil/others.tsx
+++ src/container/StudentCouncil/others.tsx
@@ -1,22 +1,27 @@
-import Image from "next/image"
-import rectangle from '@/assets/Rectangle 70.png'
+import React from 'react'
+import TopicCard from "./topicCards"
 
-const Others = () => {
+function Others() {
   return (
-    <div>
-        <h2 className="text-heading-4 ml-5 md:ml-10 lg:ml-20 text-primary-maroon">OTHEER TOPICS TO EXPLORE</h2>
-        <div className="lg:flex md:flex justify-between my-10">
-            <div className="my-5 ml-5 md:ml-10 lg:ml-20 mr-5 md:w-1/2 lg:w-1/2">
-                <div className="overflow-hidden"><Image src={rectangle} className="scale-125 translate-y-15 translate-x-10" alt="empty rectangle"/></div>
-                <div className="text-heading-6 text-gray-100 text-center bg-primary-maroon p-5 md:p-10 lg:p-10">AWAKENING<br/>CREATIVITY</div>
-            </div>
-            <div className="my-5 ml-5 md:ml-10 lg:ml-20 mr-5 md:w-1/2 lg:w-1/2">
-                <div className="overflow-hidden"><Image src={rectangle} className="scale-125 translate-y-15 translate-x-10" alt="empty rectangle"/></div>
-                <div className="text-heading-6 text-gray-100 text-center bg-primary-maroon p-5 md:p-10 lg:p-10">STUDENT<br/>COUNCIL</div>
-            </div>
-        </div>
-    </div>
-  )
+      <>
+          <div className="w-[90%] mx-auto my-20 md:mt-20">
+              <p className="uppercase text-heading-4 text-primary-maroon-1">
+                  Other Topics To explore
+              </p>
+              <div className="md:flex md:gap-11 mt-10">
+                  <TopicCard
+                      text={"AWEKENING CREATIVITY"}
+                      overlayText={"AWEKENING CREATIVITY"}
+                  />
+                  <TopicCard
+                      text={"STUDENT COUNCIL"}
+                      overlayText={"STUDENT COUNCIL"}
+                  />
+              </div>
+          </div>
+      </>
+  );
 }
 
 export default Others
+
