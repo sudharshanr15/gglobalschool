388c34ce23d03104b126f0663a8487be731a3b4d
p-karthikeyan <keyan4676@gmail.com>
2023-07-23 15:16:46 +0530

    initial setup whyus

---
+++ src/app/whyus/page.tsx
@@ -0,0 +1,12 @@
+import {Description} from "@/container/whyus"
+
+
+const Whyus = () => {
+  return (
+    <div>
+      <Description/>
+    </div>
+  )
+}
+
+export default Whyus
---
+++ src/container/whyus/description.tsx
@@ -0,0 +1,15 @@
+
+
+const Description = () => {
+  return (
+    <div className="p-16">
+      <p className="font-bold text-2xl">Early and primary year education is the foremost and basic right of every child.
+       It sets a strong foundation for learning and early development of skills.
+        GGS ensures this through <span className="text-primary-maroon">inquiry based learning and conceptual understanding</span>. 
+      We also have a remarkable set of educator team to implement this in our curriculum
+    </p>
+    </div>
+  )
+}
+
+export default Description
