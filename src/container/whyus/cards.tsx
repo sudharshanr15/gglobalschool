742a02ab41a33ad69abc7817b889f9ac7063d0af
p-karthikeyan <keyan4676@gmail.com>
2023-07-30 12:39:29 +0530

    little alterations in cards

--- src/container/whyus/cards.tsx
+++ src/container/whyus/cards.tsx
@@ -13,9 +13,9 @@ import last from '@/assets/last.png'
 const Cards = () => {
     
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-5 md:mx-20 lg:mx-20">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-5 md:mx-20 lg:mx-20">
 
-    <div className="w-[100%] hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 lg:hover:grid-rows-5 hover:text-gray-100 mx-5 mx-auto">
+    <div className="w-[100%] ease-in duration-500 hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 hover:lg:grid-rows-5 hover:text-gray-100 mx-5 mx-auto">
         <div className="image-container w-[100%] h-72 md:h-full row-span-2 lg:h-full overflow-hidden">
             <div className="relative w-full h-full">
             <Image src={grid1} alt="grid1" className="absolute inset-0 w-full h-full object-center object-cover"/>
@@ -28,94 +28,112 @@ const Cards = () => {
          </div>
     </div>
 
-    <div className="w-[100%] mx-auto px-5">
-        <div className="image-container w-[100%]  h-72 md:h-96 lg:h-[520px] overflow-hidden">
+    <div className="w-[100%] ease-in duration-500 hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 hover:lg:grid-rows-5 hover:text-gray-100 mx-5 mx-auto">
+        <div className="image-container w-[100%] h-72 md:h-full row-span-2 lg:h-full overflow-hidden">
             <div className="relative w-full h-full">
             <Image src={grid2} alt="grid1" className="absolute inset-0 w-full h-full object-center object-cover"/>
             </div>
         </div>
+        <div className="row-span-2">
         <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">GLOBAL CITIZEN</h1>
         <p className="pl-5 pr-5 mb-20">Cultivate global citizenship by promoting cultural understanding, empathy, and a sense of responsibility towards the global community.</p>
+        </div>
     </div>
 
-    <div className="w-[100%] mx-auto px-5">
-        <div className="image-container w-[100%] h-72 md:h-96 lg:h-[520px] overflow-hidden">
+    <div className="w-[100%] ease-in duration-500 hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 hover:lg:grid-rows-5 hover:text-gray-100 mx-5 mx-auto">
+        <div className="image-container w-[100%] h-72 md:h-full row-span-2 lg:h-full overflow-hidden">
             <div className="relative w-full h-full">
             <Image src={grid3} alt="grid1" className="absolute inset-0 w-full h-full object-center object-cover"/>
             </div>
         </div>
+        <div className="row-span-2">
         <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">LEARNING WITH RELEVANCE</h1>
         <p className="pl-5 pr-5 mb-20">Emphasize learning that is meaningful and applicable to real-world situations, enabling students to connect their knowledge to practical contexts.</p>
+        </div>
     </div>
 
-    <div className="w-[100%] mx-auto px-5">
-        <div className="image-container w-[100%] h-72 md:h-96 lg:h-[520px] overflow-hidden">
+    <div className="w-[100%] ease-in duration-500 hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 hover:lg:grid-rows-5 hover:text-gray-100 mx-5 mx-auto">
+        <div className="image-container w-[100%] h-72 md:h-full row-span-2 lg:h-full overflow-hidden">
             <div className="relative w-full h-full">
             <Image src={grid4} alt="grid1" className="absolute inset-0 w-full h-full object-center object-cover"/>
             </div>
         </div>
+        <div className="row-span-2">
         <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">CHARACTER BUILDING</h1>
         <p className="pl-5 pr-5 mb-20">Focus on developing strong character traits such as resilience, empathy, integrity, and leadership, to nurture well-rounded individuals.</p>
+        </div>
     </div>
 
-    <div className="w-[100%] mx-auto px-5">
-        <div className="image-container w-[100%] h-72 md:h-96 lg:h-[520px] overflow-hidden">
+    <div className="w-[100%] ease-in duration-500 hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 hover:lg:grid-rows-5 hover:text-gray-100 mx-5 mx-auto">
+        <div className="image-container w-[100%] h-72 md:h-full row-span-2 lg:h-full overflow-hidden">
             <div className="relative w-full h-full">
             <Image src={grid5} alt="grid1" className="absolute inset-0 w-full h-full object-center object-cover"/>
             </div>
         </div>
+        <div className="row-span-2">
         <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">SUCCESS FOR ALL</h1>
         <p className="pl-5 pr-5 mb-20">Ensure that every student has equal opportunities and support to achieve their full potential, regardless of their background or abilities.</p>
+        </div>
     </div>
 
-    <div className="w-[100%] mx-auto px-5">
-        <div className="image-container w-[100%] h-72 md:h-96 lg:h-[520px] overflow-hidden">
+    <div className="w-[100%] ease-in duration-500 hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 hover:lg:grid-rows-5 hover:text-gray-100 mx-5 mx-auto">
+        <div className="image-container w-[100%] h-72 md:h-full row-span-2 lg:h-full overflow-hidden">
             <div className="relative w-full h-full">
             <Image src={grid6} alt="grid1" className="absolute inset-0 w-full h-full object-center object-cover"/>
             </div>
         </div>
+        <div className="row-span-2">
         <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">RESPONSIBLE LEARNERS</h1>
         <p className="pl-5 pr-5 mb-20">Instill a sense of responsibility and accountability in students towards their own learning, their actions, and the environment.</p>
+        </div>
     </div>
 
-    <div className="w-[100%] mx-auto px-5">
-        <div className="image-container w-[100%] h-72 md:h-96 lg:h-[520px] overflow-hidden">
+    <div className="w-[100%] ease-in duration-500 hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 hover:lg:grid-rows-5 hover:text-gray-100 mx-5 mx-auto">
+        <div className="image-container w-[100%] h-72 md:h-full row-span-2 lg:h-full overflow-hidden">
             <div className="relative w-full h-full">
             <Image src={grid7} alt="grid1" className="absolute inset-0 w-full h-full object-center object-cover"/>
             </div>
         </div>
+        <div className="row-span-2">
         <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">SERVICE</h1>
         <p className="pl-5 pr-5 mb-20">Encourage students to actively contribute to their communities through meaningful service projects, fostering a spirit of compassion and civic engagement.</p>
+        </div>
     </div>
 
-    <div className="w-[100%] mx-auto px-5">
-        <div className="image-container w-[100%] h-72 md:h-96 lg:h-[520px] overflow-hidden">
+    <div className="w-[100%] ease-in duration-500 hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 hover:lg:grid-rows-5 hover:text-gray-100 mx-5 mx-auto">
+        <div className="image-container w-[100%] h-72 md:h-full row-span-2 lg:h-full overflow-hidden">
             <div className="relative w-full h-full">
             <Image src={grid8} alt="grid1" className="absolute inset-0 w-full h-full object-center object-cover"/>
             </div>
         </div>
+        <div className="row-span-2">
         <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">INQUIRY BASED EDUCATION</h1>
         <p className="pl-5 pr-5 mb-20">Promote an inquiry-based approach to learning, where students are encouraged to ask questions, explore, and discover knowledge through curiosity-driven exploration.</p>
+        </div>
     </div>
 
-    <div className="w-[100%] mx-auto px-5">
-        <div className="image-container w-[100%] h-72 md:h-96 lg:h-[520px] overflow-hidden">
+    <div className="w-[100%] ease-in duration-500 hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 hover:lg:grid-rows-5 hover:text-gray-100 mx-5 mx-auto">
+        <div className="image-container w-[100%] h-72 md:h-full row-span-2 lg:h-full overflow-hidden">
             <div className="relative w-full h-full">
             <Image src={grid9} alt="grid1" className="absolute inset-0 w-full h-full object-center object-cover"/>
             </div>
         </div>
+        <div className="row-span-2">
         <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">HOLISTIC DEVLOPEMENT</h1>
         <p className="pl-5 pr-5 mb-20">Foster the holistic development of students, addressing their intellectual, emotional, physical, and social well-being, to nurture their overall growth.</p>
+        </div>
     </div>
 
-   <div className="lg:w-1/3 mx-auto px-5 lg:col-span-3">
-        <div className="image-container w-[100%] h-72 md:h-96 lg:h-96 overflow-hidden">
+   <div className="w-[100%] lg:w-1/3 ease-in duration-500 hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 hover:lg:grid-rows-5 hover:text-gray-100 mx-auto mx-5 lg:col-span-3">
+        <div className="image-container w-[100%] h-72 md:h-full row-span-2 lg:h-full overflow-hidden">
             <div className="relative w-full h-full">
             <Image src={last} alt="grid1" className="absolute inset-0 w-full h-full object-center object-cover"/>
             </div>
         </div>
+        <div className="row-span-2">
         <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">PERSONAL & SOCIAL INTEGRITY</h1>
         <p className="pl-5 pr-5 mb-20">Cultivate personal and social integrity, emphasizing ethical behavior, honesty, respect, and the development of positive relationships within the school community.</p>
+        </div>
     </div>
 
     </div>
