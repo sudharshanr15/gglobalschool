14571df07c219e6adaf15a87a841401d90739831
p-karthikeyan <keyan4676@gmail.com>
2023-08-01 21:15:49 +0530

    spacings updated in community page

--- src/container/community/onetwo.tsx
+++ src/container/community/onetwo.tsx
@@ -1,21 +1,21 @@
 const Onetwo = () => {
   return (
-    <div className="px-5 md:px-10 lg:px-20 py-5 pb-10 md:py-10 lg:py-20 bg-primary-maroon text-gray-100 md:flex lg:flex md:gap-10 lg:gap-20">
+    <section className="px-md md:px-xl lg:px-3xl py-md pb-xl md:py-xl lg:py-3xl bg-primary-maroon text-gray-100 md:flex lg:flex md:gap-xl lg:gap-3xl">
     
     <div>
-      <h1 className="text-heading-5 pt-5 pb-10">1. BE THE CHANGE</h1>
+      <h1 className="text-heading-5 pt-md pb-xl">1. BE THE CHANGE</h1>
       <p>Be the change you want to see in the world and start locally. We make
 our students understand about our community and it’s people and there
 by able to showcase their best interest in their well-being.</p>  
     </div>
     <div>
-      <h1 className="text-heading-5 pt-5 pb-10">2. ACKNOWLEDGE AND RESPECT</h1>
+      <h1 className="text-heading-5 pt-md pb-xl">2. ACKNOWLEDGE AND RESPECT</h1>
       <p>Acknowledge and respect the individuality of each. Every community
 is different and these differences must be understood and honoured in
 order to feel a sense of peace and belongingness.</p> 
     </div>
         
-    </div>
+    </section>
   )
 }
 
--- src/container/community/wellness.tsx
+++ src/container/community/wellness.tsx
@@ -1,13 +1,13 @@
 const Wellness = () => {
   return (
-    <div className="px-5 md:px-10 lg:px-20 py-10 md:py-10 lg:py-20">
-      <h1 className="text-heading-4 text-primary-maroon py-5">COMMUNITY WELLNESS</h1>
+    <section className="px-md md:px-xl lg:px-3xl py-xl md:py-xl lg:py-3xl">
+      <h1 className="text-heading-4 text-primary-maroon py-md">COMMUNITY WELLNESS</h1>
       <p>G global school promotes and motivates students to become life long
 learner’s and a responsible citizens. As a step towards it we incorporate
 community wellness in our curriculum there by making the students
 understand the purpose of learning and it’s application in the real world.</p><br/>
 <p>These are few steps we take in our school to bring community wellness,</p>
-    </div>
+    </section>
   )
 }
 
