4660bdfdbd4bf2ed26cc5bb268873ccd31eb4e5e
p-karthikeyan <keyan4676@gmail.com>
2023-08-01 21:38:16 +0530

    spacings updates in whyus page

--- src/container/whyus/cards.tsx
+++ src/container/whyus/cards.tsx
@@ -13,7 +13,7 @@ import last from '@/assets/last.png'
 const Cards = () => {
     
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-5 md:mx-20 lg:mx-20">
+    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-xl mx-md md:mx-3xl lg:mx-3xl">
 
     <div className="w-[100%] ease-in duration-500 hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 hover:lg:grid-rows-5 hover:text-gray-100 mx-5 mx-auto">
         <div className="image-container w-[100%] h-72 md:h-full row-span-2 lg:h-full overflow-hidden">
@@ -22,8 +22,8 @@ const Cards = () => {
             </div>
         </div>
         <div className="row-span-3">
-        <h1 className="font-bold text-3xl pt-5 pb-2 pl-4 text-primary-orange">CRITICAL THINKING</h1>
-        <p className="px-5 pb-20">Foster critical thinking skills and open-mindedness to encourage analytical reasoning, problem-solving,
+        <h1 className="font-bold text-3xl pt-md pb-md pl-md text-primary-orange">CRITICAL THINKING</h1>
+        <p className="px-md pb-3xl">Foster critical thinking skills and open-mindedness to encourage analytical reasoning, problem-solving,
          and the ability to consider different perspectives. </p>
          </div>
     </div>
@@ -35,8 +35,8 @@ const Cards = () => {
             </div>
         </div>
         <div className="row-span-2">
-        <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">GLOBAL CITIZEN</h1>
-        <p className="pl-5 pr-5 mb-20">Cultivate global citizenship by promoting cultural understanding, empathy, and a sense of responsibility towards the global community.</p>
+        <h1 className="font-bold text-3xl mt-md mb-md pl-md text-primary-orange">GLOBAL CITIZEN</h1>
+        <p className="pl-md pr-md mb-3xl">Cultivate global citizenship by promoting cultural understanding, empathy, and a sense of responsibility towards the global community.</p>
         </div>
     </div>
 
@@ -47,8 +47,8 @@ const Cards = () => {
             </div>
         </div>
         <div className="row-span-2">
-        <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">LEARNING WITH RELEVANCE</h1>
-        <p className="pl-5 pr-5 mb-20">Emphasize learning that is meaningful and applicable to real-world situations, enabling students to connect their knowledge to practical contexts.</p>
+        <h1 className="font-bold text-3xl mt-md mb-md pl-md text-primary-orange">LEARNING WITH RELEVANCE</h1>
+        <p className="pl-md pr-md mb-3xl">Emphasize learning that is meaningful and applicable to real-world situations, enabling students to connect their knowledge to practical contexts.</p>
         </div>
     </div>
 
@@ -59,8 +59,8 @@ const Cards = () => {
             </div>
         </div>
         <div className="row-span-2">
-        <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">CHARACTER BUILDING</h1>
-        <p className="pl-5 pr-5 mb-20">Focus on developing strong character traits such as resilience, empathy, integrity, and leadership, to nurture well-rounded individuals.</p>
+        <h1 className="font-bold text-3xl mt-md mb-md pl-md text-primary-orange">CHARACTER BUILDING</h1>
+        <p className="pl-md pr-md mb-3xl">Focus on developing strong character traits such as resilience, empathy, integrity, and leadership, to nurture well-rounded individuals.</p>
         </div>
     </div>
 
@@ -71,8 +71,8 @@ const Cards = () => {
             </div>
         </div>
         <div className="row-span-2">
-        <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">SUCCESS FOR ALL</h1>
-        <p className="pl-5 pr-5 mb-20">Ensure that every student has equal opportunities and support to achieve their full potential, regardless of their background or abilities.</p>
+        <h1 className="font-bold text-3xl mt-md mb-md pl-md text-primary-orange">SUCCESS FOR ALL</h1>
+        <p className="pl-md pr-md mb-3xl">Ensure that every student has equal opportunities and support to achieve their full potential, regardless of their background or abilities.</p>
         </div>
     </div>
 
@@ -83,8 +83,8 @@ const Cards = () => {
             </div>
         </div>
         <div className="row-span-2">
-        <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">RESPONSIBLE LEARNERS</h1>
-        <p className="pl-5 pr-5 mb-20">Instill a sense of responsibility and accountability in students towards their own learning, their actions, and the environment.</p>
+        <h1 className="font-bold text-3xl mt-md mb-md pl-md text-primary-orange">RESPONSIBLE LEARNERS</h1>
+        <p className="pl-md pr-md mb-3xl">Instill a sense of responsibility and accountability in students towards their own learning, their actions, and the environment.</p>
         </div>
     </div>
 
@@ -95,8 +95,8 @@ const Cards = () => {
             </div>
         </div>
         <div className="row-span-2">
-        <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">SERVICE</h1>
-        <p className="pl-5 pr-5 mb-20">Encourage students to actively contribute to their communities through meaningful service projects, fostering a spirit of compassion and civic engagement.</p>
+        <h1 className="font-bold text-3xl mt-md mb-md pl-md text-primary-orange">SERVICE</h1>
+        <p className="pl-md pr-md mb-3xl">Encourage students to actively contribute to their communities through meaningful service projects, fostering a spirit of compassion and civic engagement.</p>
         </div>
     </div>
 
@@ -107,8 +107,8 @@ const Cards = () => {
             </div>
         </div>
         <div className="row-span-2">
-        <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">INQUIRY BASED EDUCATION</h1>
-        <p className="pl-5 pr-5 mb-20">Promote an inquiry-based approach to learning, where students are encouraged to ask questions, explore, and discover knowledge through curiosity-driven exploration.</p>
+        <h1 className="font-bold text-3xl mt-md mb-md pl-md text-primary-orange">INQUIRY BASED EDUCATION</h1>
+        <p className="pl-md pr-md mb-3xl">Promote an inquiry-based approach to learning, where students are encouraged to ask questions, explore, and discover knowledge through curiosity-driven exploration.</p>
         </div>
     </div>
 
@@ -119,24 +119,24 @@ const Cards = () => {
             </div>
         </div>
         <div className="row-span-2">
-        <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">HOLISTIC DEVLOPEMENT</h1>
-        <p className="pl-5 pr-5 mb-20">Foster the holistic development of students, addressing their intellectual, emotional, physical, and social well-being, to nurture their overall growth.</p>
+        <h1 className="font-bold text-3xl mt-md mb-md pl-md text-primary-orange">HOLISTIC DEVLOPEMENT</h1>
+        <p className="pl-md pr-md mb-3xl">Foster the holistic development of students, addressing their intellectual, emotional, physical, and social well-being, to nurture their overall growth.</p>
         </div>
     </div>
 
-   <div className="w-[100%] lg:w-1/3 ease-in duration-500 hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 hover:lg:grid-rows-5 hover:text-gray-100 mx-auto mx-5 lg:col-span-3">
+   <div className="w-[100%] lg:w-1/3 ease-in duration-500 hover:bg-primary-maroon grid md:grid-rows-3 md:hover:grid-rows-5 lg:grid-rows-3 hover:lg:grid-rows-5 hover:text-gray-100 mx-5 mx-auto lg:col-span-3">
         <div className="image-container w-[100%] h-72 md:h-full row-span-2 lg:h-full overflow-hidden">
             <div className="relative w-full h-full">
             <Image src={last} alt="grid1" className="absolute inset-0 w-full h-full object-center object-cover"/>
             </div>
         </div>
         <div className="row-span-2">
-        <h1 className="font-bold text-3xl mt-5 mb-2 pl-4 text-primary-orange">PERSONAL & SOCIAL INTEGRITY</h1>
-        <p className="pl-5 pr-5 mb-20">Cultivate personal and social integrity, emphasizing ethical behavior, honesty, respect, and the development of positive relationships within the school community.</p>
+        <h1 className="font-bold text-3xl mt-md mb-md pl-md text-primary-orange">PERSONAL & SOCIAL INTEGRITY</h1>
+        <p className="pl-md pr-md mb-3xl">Cultivate personal and social integrity, emphasizing ethical behavior, honesty, respect, and the development of positive relationships within the school community.</p>
         </div>
     </div>
 
-    </div>
+    </section>
   )
 }
 
