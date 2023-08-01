670249cfab5719d252d4dce502f264bff565d5c4
p-karthikeyan <keyan4676@gmail.com>
2023-08-01 21:45:59 +0530

    spacings updateed in work with us

--- src/container/workwithus/content.tsx
+++ src/container/workwithus/content.tsx
@@ -4,11 +4,11 @@ import benefitImage from '@/assets/student council benefits.png'
 
 const Content = () => {
   return (
-    <div className="w-full md:mt-20 lg:mt-20">
-      <div className="mx-5 md:mx-10 lg:mx-20">
+    <section className="w-full md:mt-3xl lg:mt-3xl">
+      <div className="mx-md md:mx-xl lg:mx-3xl">
       <Image className="w-full" src={contentimage} alt="why us image"/>
       </div>
-      <div className="m-5 md:m-10 lg:m-20"><p>
+      <div className="m-md md:m-xl lg:m-3xl"><p>
       At GGS, we seek persons who have the love for learning, willing to
 explore and be a part of the dynamic and vibrant team.
 </p><br/><p>
@@ -32,7 +32,7 @@ environment are some of the benefits of G Global School culture.
 To apply, please click the below box and submit the duly filled form.
 </p>
       </div>
-    </div>
+    </section>
   )
 }
 
--- src/container/workwithus/descriptionBox.tsx
+++ src/container/workwithus/descriptionBox.tsx
@@ -1,8 +1,8 @@
 const DescriptionBox = () => {
   return (
-    <div className="m-5 lg:mx-32 mx-30 text-heading-6 lg:text-heading-5 text-gray-100 text-center rounded-3xl p-6 lg:p-20 bg-primary-maroon">
+    <section className="m-md lg:mx-3xl mx-3xl text-heading-6 lg:text-heading-5 text-gray-100 text-center rounded-3xl p-6 lg:p-3xl bg-primary-maroon">
       “Knowing yourself is the beginning of all wisdom” - Aristotle.
-    </div>
+    </section>
   )
 }
 
