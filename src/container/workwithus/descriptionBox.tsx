0249c05f147def5129ea1cd0985a52d504e088e3
p-karthikeyan <keyan4676@gmail.com>
2023-08-08 18:20:53 +0530

    whyus, workwithus page updated!

--- src/container/workwithus/descriptionBox.tsx
+++ src/container/workwithus/descriptionBox.tsx
@@ -1,7 +1,9 @@
 const DescriptionBox = () => {
   return (
-    <section className="m-md lg:mx-3xl mx-3xl text-heading-6 lg:text-heading-5 text-gray-100 text-center rounded-3xl p-6 lg:p-3xl bg-primary-maroon">
+    <section className="section-container">
+    <div className="text-heading-6 lg:text-heading-5 text-gray-100 text-center rounded-3xl p-6 lg:p-3xl bg-primary-maroon">
       “Knowing yourself is the beginning of all wisdom” - Aristotle.
+    </div>
     </section>
   )
 }
