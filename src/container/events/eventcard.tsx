c799310ca76737f9db010b365b262834aedcd075
p-karthikeyan <keyan4676@gmail.com>
2023-08-23 21:04:32 +0530

    list of events done completely

--- src/container/events/eventcard.tsx
+++ src/container/events/eventcard.tsx
@@ -2,8 +2,21 @@ import Image from "next/image"
 
 const Eventcard = ({title}) => {
   return (
-    <div className="w-fit bg-gray-100">
-        <Image src={'/eventsimg/'+title+'.png'} width={300} height={100} alt={title}/>
+    <div className="w-full h-fit bg-gray-100">
+        <div className="relative h-40">
+        <Image src={'/eventsimg/'+title+'.png'} className="w-full h-full" width={200} height={200} alt={title}/>
+        <div className="absolute flex flex-col justify-center items-center bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden opacity-0 transition duration-700 ease-in-out hover:opacity-100 text-center">
+                    <div className="relative w-full h-full">
+                    <Image src={'/eventsimg/DEAR WEEK.png'} className="z-1 absolute brightness-50 w-full h-full" width={200} height={200} alt={title}/>
+                    <div className="absolute  inset-0 flex items-center justify-center">
+                    <button className=" w-fit h-fit bg-primary-yellow-1 z-2 hover:bg-white rounded-full text-xl font-bold lg:text-heading-6 py-4 px-md lg:px-large">
+                      View Gallery
+                    </button>
+                    </div>
+                    </div>
+                    
+          </div>
+        </div>
         <p className="p-md font-bold">{title}</p>
     </div>
   )
--- src/container/events/listofevents.tsx
+++ src/container/events/listofevents.tsx
@@ -3,18 +3,10 @@ import Eventcard from './eventcard'
 const Listofevents = () => {
 
   const events=[
-    "Fathers Day","International yoga day","International mud day","National doctors day",
-    "World chocolate day","Investiture ceremony","International friendship day and twins day",
-    "Independence Day",
-    "Krishna Jayanthi and photography day","Vinayak Chaturthi","Teachers day and Onam",
-    "International literacy day",
-    "Grand parents day","Dot day","GGS Book fair","Navarathri begins","Vijayadashami","Sports day","Diwali",
-    "Halloween day",
-    "GGS Art competition","Children’s day","World heritage week","Thanks giving day","Math expo",
-    "Christmas and new year celebration",
-    "Pongal celebration","Republic Day","4th GGS kiddathon","DEAR WEEK","International mother language day",
-    "GGS SANGAMAM",
-    "International women’s day","Art exhibition","Annual day  graduation","PYP Exhibition"
+    "Fathers Day","International yoga day","International mud day","National doctors day","World chocolate day","Investiture ceremony","International friendship day and twins day","Independence Day",
+    "Krishna Jayanthi and photography day","Vinayak Chaturthi","Teachers day and Onam","International literacy day","Grand parents day","Dot day","GGS Book fair","Navarathri begins","Vijayadashami","Sports day","Diwali","Halloween day",
+    "GGS Art competition","Children’s day","World heritage week","Thanks giving day","Math expo","Christmas and new year celebration",
+    "Pongal celebration","Republic Day","4th GGS kiddathon","DEAR WEEK","International mother language day","GGS SANGAMAM","International women’s day","Art exhibition","Annual day  graduation","PYP Exhibition"
   ]
   const content=events.map(val=>{
     return <Eventcard title={val}/>
@@ -23,7 +15,7 @@ const Listofevents = () => {
   return (
     <section className="section-container">
       <p className="text-heading-4 text-primary-maroon mb-large">LIST OF DAYS & EVENTS -2023</p>
-      <div className='grid gap-md lg:gap-2xl grid-cols-2 lg:grid-cols-4'>
+      <div className='lg:my-xl grid gap-md lg:gap-2xl grid-cols-2 lg:grid-cols-4'>
       {content}
       </div>
     </section>
