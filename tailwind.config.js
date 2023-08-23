30c891c2a9be82d4e6d7146c5cd64187d5d35415
Sudharshan <sudharshanrms2004@gmail.com>
2023-08-23 22:10:37 +0530

    Added font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,6 +10,10 @@ module.exports = {
     './src/container/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Montserrat', ...defaultTheme.fontFamily.sans],
+      'Montserrat': ["Montserrat", ...defaultTheme.fontFamily.sans]
+    },
     extend: {
       colors: {
         "primary-maroon": {
