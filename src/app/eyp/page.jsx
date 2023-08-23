466163a54b3894ec1bcf02e530a6380517ee2200
Sudharshan <sudharshanrms2004@gmail.com>
2023-08-23 20:40:48 +0530

    Modified eyp page

--- src/app/eyp/page.jsx
+++ src/app/eyp/page.jsx
@@ -5,6 +5,7 @@ import eypLogo2 from "../../assets/Group 24.png";
 import TopicCard from "@/component/topicsCard/TopicCard";
 import Hero from "../../component/hero/Hero";
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
+import {EarlyYears, Offers} from "@/container/eyp/";
 
 function page() {
     return (
@@ -15,27 +16,27 @@ function page() {
                     <FormatQuoteIcon sx={{ fontSize: 150, opacity: 0.2, color: "#F05A22"}} />
                 </div>
                 <p className="text-heading-6 uppercase text-white z-10">
-                    SHAPING YOUNG MINDS : A CHILD'S LEARNING BEGINS FROM EARLY
-                    YEARS
+                    Our goal is to promote <span className="text-primary-yellow-1">“International mindedness”</span>, manifesting a sense of morality to all the human beings in the global society.
                 </p>
             </section>
 
-            {/* early year program */}
-
-            <section className="section-container w-full flex flex-col md:flex-row gap-5 md:gap-28">
+            <section className="section-container w-full flex flex-col md:flex-row gap-5 md:gap-xl">
                 <div className="w-full md:w-1/2">
                     <Image
                         src={eypLogo}
-                        className="w-full h-full"
+                        className="w-full h-full object-cover"
                         alt="logo"
                         unoptimized
                     />
                 </div>
                 <div className="w-full md:w-1/2">
-                    <p className="text-primary-maroon-1 text-heading-3 mb-xl">
-                        EARLY YEAR PROGRAMME
-                    </p>
-                    <div className="flex flex-col gap-large text-subheading-regular">
+                    <h2 className="text-primary-maroon-1 text-heading-3 mb-large uppercase">
+                        PRIMARY YEAR PROGRAMME
+                    </h2>
+                    <h4 className="text-heading-6 p-md bg-primary-maroon-1 text-white mb-large">
+                        Curiosity is the fuel for discovery, inquiry and learning
+                    </h4>
+                    <div className="flex flex-col gap-large text-body">
                         <p>
                             Children are natural inquirers from birth; they have
                             the capacity to learn about, interact with, and
@@ -56,14 +57,59 @@ function page() {
                 </div>
             </section>
 
-            {/* feature */}
+            <Offers />
 
-            <section className="section-container flex flex-col md:flex-row gap-5 md:gap-28">
+            <section className="section-container py-0">
+                <div className="bg-primary-yellow-1 p-xl">
+                    <h2 className="text-primary-maroon-1 text-heading-3 uppercase text-center">
+                        SHARPING YOUNG MINDS
+                    </h2>
+                </div>
+            </section>
+
+            <section className="section-container w-full flex flex-col md:flex-row gap-5 md:gap-xl">
                 <div className="w-full md:w-1/2">
-                    <p className="text-primary-maroon-1 text-heading-3 mb-xl">
+                    <Image
+                        src={eypLogo}
+                        className="w-full h-full object-cover"
+                        alt="logo"
+                        unoptimized
+                    />
+                </div>
+                <div className="w-full md:w-1/2">
+                    <h2 className="text-primary-maroon-1 text-heading-3 mb-large uppercase">
+                        EARLY YEAR PROGRAMME
+                    </h2>
+                    <h4 className="text-heading-6 p-md bg-primary-maroon-1 text-white mb-large">
+                        A child’s learning begins from early years.
+                    </h4>
+                    <div className="flex flex-col gap-large text-body">
+                        <p>
+                            Children are natural inquirers from birth; they have
+                            the capacity to learn about, interact with, and
+                            interpret the world around them.
+                        </p>
+                        <p>
+                            From birth, children possess all kinds of mental
+                            abilities uniquely suited to these early phases of
+                            learning and development.
+                        </p>
+                        <p>
+                            They are curious and capable learners with a sense
+                            of agency, rich in potential, bringing valid skills,
+                            preferences, and understanding to the educational
+                            process.
+                        </p>
+                    </div>
+                </div>
+            </section>
+
+            <section className="section-container flex flex-col md:flex-row gap-5 md:gap-xl">
+                <div className="w-full md:w-1/2">
+                    <p className="text-primary-maroon-1 text-heading-3 mb-large uppercase">
                         FEATURES
                     </p>
-                    <div className="flex flex-col gap-large text-subheading-regular">
+                    <div className="flex flex-col gap-large text-body">
                         <p>
                             Learning spaces at GGS are designed to support
                             active participation of children through play.
@@ -95,43 +141,12 @@ function page() {
             </section>
 
             {/* PROGRAMS STECTION */}
-            <section className="section-container">
-                <h2 className="text-heading-4 md:text-heading-3 uppercase lg:w-[90%] text-primary-maroon-1 mb-xl">
-                    early years @ g global school programmes
-                </h2>
-
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-xl">
-                    <div className="bg-[#790000] text-white rounded-3xl p-xl">
-                        <h3 className="text-heading-4 mb-xl uppercase">PLAY GROUND</h3>
-                        <p className="text-primary-yellow text-heading-6 uppercase">
-                            2.5 to 3 years
-                        </p>
-                    </div>
-                    <div className="bg-[#790000] text-white rounded-3xl p-xl">
-                        <h3 className="text-heading-4 mb-xl uppercase">KINDER GARDEN 1</h3>
-                        <p className="text-primary-yellow text-heading-6 uppercase">
-                            3 years
-                        </p>
-                    </div>
-                    <div className="bg-[#790000] text-white rounded-3xl p-xl">
-                        <h3 className="text-heading-4 mb-xl uppercase">KINDER GARDEN 2</h3>
-                        <p className="text-primary-yellow text-heading-6 uppercase">
-                            4 years
-                        </p>
-                    </div>
-                    <div className="bg-[#790000] text-white rounded-3xl p-xl">
-                        <h3 className="text-heading-4 mb-xl uppercase">KINDER GARDEN 3</h3>
-                        <p className="text-primary-yellow text-heading-6 uppercase">
-                            5 years
-                        </p>
-                    </div>
-                </div>
-            </section>
+            <EarlyYears />
 
             {/* other topics to explore */}
 
             <section className="section-container">
-                <h2 className="text-heading-4 md:text-heading-3 uppercase lg:w-[90%] text-primary-maroon-1 mb-xl">
+                <h2 className="text-heading-4 md:text-heading-3 uppercase lg:w-[90%] text-primary-maroon-1 mb-large">
                     OTHER TOPICS TO EXPLORE
                 </h2>
                 <div className="flex flex-col md:flex-row gap-xl">
---
+++ src/container/eyp/Offers.tsx
@@ -0,0 +1,121 @@
+import Image, { StaticImageData } from "next/image";
+import img from "@/assets/Rectangle 75.png";
+
+type OffersType = {
+  img: StaticImageData;
+  title: string;
+  text: string [];
+};
+
+function Offers() {
+  const offers_1: OffersType[] = [
+    {
+      img: img,
+      title: "ESSENCE OF PYP",
+      text: [
+        "Early learning in the PYP is a holistic learning experience that integrates socio -emotional, physical and cognitive development.",
+        "In the PYP classroom, it takes place in dynamic environments that promote play, discovery and exploration.",
+      ],
+    },
+    {
+      img: img,
+      title: "ENCOURAGING STUDENTS TO DEVELOP SKILLS",
+      text: [
+        "We offer a comprehensive, concept driven, student - centric and inquiry-based approach to teaching and learning that fosters the development of thinking, communication, self management, social and research skills.",
+      ],
+    },
+    {
+      img: img,
+      title: "HANDS ON EXPERIENCE",
+      text: [
+        "The learners are engaged in challenging, relevant, critical and creative thinking activities. We also ensure student - centric learning engagements.",
+      ],
+    },
+  ];
+
+  const offers_2: OffersType[] = [
+    {
+        img: img,
+        title: "CONCEPT DRIVEN INQUIRY",
+        text: [
+            "The learners explore the essential elements in the unit, reflect their learning through student agency, assess their learning through self and peer assessments which supports student agency and also make connections to real world experiences."
+        ]
+    },
+    {
+        img: img,
+        title: "Caring and responsible citizens",
+        text: [
+            "The units of inquiry are designed to help foster their imaginations and meet new challenges and opportunities to help build a peaceful world."
+        ]
+    }
+  ]
+
+  return (
+    <section className="section-container">
+      <h2 className="text-primary-maroon-1 text-heading-3 mb-large uppercase">
+        WHAT WE OFFER
+      </h2>
+      <div className="flex flex-col gap-md text-body mb-large">
+        <p>
+          Learning spaces at GGS are designed to support active participation of
+          the children through play.
+        </p>
+        <p>
+          Learning engagements and classroom activities are designed by teachers
+          to develop their fine and gross motor skills in every day teaching and
+          learning.
+        </p>
+      </div>
+      <div className="flex flex-col gap-xl">
+        <div className="flex gap-md justify-center flex-wrap md:flex-nowrap">
+            {offers_1.map((item, index) => (
+                <div className="p-md bg-gray-100 w-full xl:max-w-[40rem]" key={index}>
+                    <div className="max-h-[14rem] overflow-hidden">
+                    <Image
+                        src={item.img}
+                        alt="Offer Image"
+                        className="h-full w-full object-cover"
+                    />
+                    </div>
+                    <h3 className="text-primary-maroon-1 uppercase text-heading-5 my-md">
+                        {item.title}
+                    </h3>
+                    <div className="flex flex-col gap-large text-gray-900">
+                        {item.text.map((item, index) => (
+                            <p key={index}>
+                                {item}
+                            </p>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div>
+        <div className="flex gap-md justify-center flex-wrap md:flex-nowrap">
+            {offers_2.map((item, index) => (
+                <div className="p-md bg-gray-100 w-full xl:max-w-[40rem]" key={index}>
+                    <div className="max-h-[14rem] overflow-hidden">
+                    <Image
+                        src={item.img}
+                        alt="Offer Image"
+                        className="h-full w-full object-cover"
+                    />
+                    </div>
+                    <h3 className="text-primary-maroon-1 uppercase text-heading-5 my-md">
+                        {item.title}
+                    </h3>
+                    <div className="flex flex-col gap-large text-gray-900">
+                        {item.text.map((item, index) => (
+                            <p key={index}>
+                                {item}
+                            </p>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Offers;
