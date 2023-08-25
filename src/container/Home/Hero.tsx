16a049e5d7fdd6de563e503a89861b6ca5fc1470
Sudharshan <sudharshanrms2004@gmail.com>
2023-08-25 23:52:17 +0530

    Modified hero section

--- src/container/Home/Hero.tsx
+++ src/container/Home/Hero.tsx
@@ -28,15 +28,15 @@ function Hero(){
 
   return (
     <section className="hero-section section-container pb-0 bg-primary-orange-1 w-full h-screen">
-      <div className="w-full h-full flex flex-col justify-between items-center md:flex-row gap-md pt-[100px] text-white">
-        <div className="w-full md:w-1/2">
+      <div className="w-full h-full flex flex-col justify-between items-center lg:flex-row gap-md pt-[100px] text-white">
+        <div className="w-full lg:w-7/12">
           <div className="flex flex-col gap-large md:gap-xl">
-            <div className="text-heading-4 md:text-heading-1 uppercase w-fit">
+            <div className="text-heading-4 sm:text-heading-3 md:text-heading-3 xl:text-heading-1 uppercase w-fit">
               <h1 className="text-primary-maroon-1">ENLIGHTENING</h1>
               <Image src={arrow_underline} alt="Underline" className="w-full h-auto" />
               <h1>MINDS</h1>
             </div>
-            <h2 className="text-[0.8rem] font-bold md:text-heading-5 uppercase">
+            <h2 className="text-caption sm:text-heading-6 font-bold md:text-heading-5 uppercase">
               G GLOBAL SCHOOL THE <span className="text-primary-maroon-1">FIRST AUTHORISED IB WORLD SCHOOL</span> IN TIRUCHENGODE.
             </h2>
             <Link href={""} className="btn-slide w-fit">
@@ -44,15 +44,17 @@ function Hero(){
             </Link>
           </div>
         </div>
-        <div className="w-full md:w-1/2 h-full">
+        <div className="w-full lg:w-5/12 h-full">
           <div className="w-full max-w-[45rem] mx-auto transition-opacity duration-1000 relative h-full">
             {hero_images.map((item, index) => (
-              <Image
-                key={index}
-                src={item}
-                alt="Hero image"
-                className={`w-full opacity-0 transition-opacity duration-200 absolute bottom-0 left-0 ${(currentImage == index) ? "opacity-100" : ""}`}
-              />
+              <div className={`flex items-end absolute bottom-0 left-0 w-full h-full xl:h-auto opacity-0 ${(currentImage == index) ? "opacity-100" : ""}`}>
+                <Image
+                  key={index}
+                  src={item}
+                  alt="Hero image"
+                  className="mx-auto w-auto h-full lg:w-full lg:h-auto transition-opacity duration-200"
+                />
+              </div>
             ))}
           </div>
         </div>
