36963bb30c04fad2438a74ed87a5780e6c2ffec5
Sudharshan <sudharshanrms2004@gmail.com>
2023-08-31 01:53:45 +0530

    Modified admission enquiry form

--- src/component/Admission-Form/Form.jsx
+++ src/component/Admission-Form/Form.jsx
@@ -1,92 +1,132 @@
-export default function Form({ onClose, onSubmit }) {
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    onSubmit();
-  };
+import CancelIcon from "@mui/icons-material/Cancel";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
-  return (
-    <div className="fixed md:w-[90%] w-[100%] inset-0 flex justify-center items-center z-50">
-      {/* close form when clicked outside the form  */}
-      
-      <div
-        className="fixed inset-0 bg-black opacity-50 z-10"
-        onClick={onClose}
-      ></div>
+export default function Form({ onClose }) {
+    const [isThankYouOpen, setThankYouOpen] = useState(false);
 
-      {/* The form */}
-      <form
-        onSubmit={handleFormSubmit}
-        className="bg-white p-10 sm:p-6 md:p-10 rounded shadow-lg w-full sm:w-3/4 md:w-1/2 lg:w-[45%] z-20 relative" 
-      >
-        <div className="flex">
-          <h2 className=" mb-4 uppercase text-primary-maroon-1 text-heading-5">
-            ENQUIRY FORM
-          </h2>
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        setThankYouOpen(true);
+    };
 
-          <button
-            onClick={onClose}
-            className="absolute top-2 right-2 bg-primary-maroon text-white w-6 h-6 rounded-full flex items-center justify-center"
-          >
-            &times;
-          </button>
-        </div>
+    function FormContainer() {
+        return (
+            <div className="fixed w-full inset-0 flex justify-center items-center z-50 section-container">
+                {/* close form when clicked outside the form  */}
 
-        <div className="mb-4">
-          <h3 className="mb-2 uppercase text-primary-maroon-1 text-subheading">
-            Student Details
-          </h3>
-          <input
-            type="text"
-            className="w-full p-2 border border-gray-600 rounded mb-2 placeholder-gray-500"
-            placeholder="Enter Student Name"
-            required
-          />
-          <input
-            type="number"
-            className="w-full p-2 border border-gray-600 rounded mb-2 placeholder-gray-500"
-            placeholder="Enter Age"
-            min="2"
-            required
-          />
-          <input
-            type="text"
-            className="w-full p-2 border border-gray-600 rounded mb-2 placeholder-gray-500"
-            placeholder="Select Programme"
-            required
-          />
-        </div>
+                <div
+                    className="fixed inset-0 bg-gray-900 opacity-70 z-10"
+                    onClick={() => onClose(false)}
+                ></div>
 
-        <div className="mb-4">
-          <h3 className=" mb-2 uppercase text-primary-maroon-1 text-subheading">
-            Parent Details
-          </h3>
-          <input
-            type="text"
-            className="w-full p-2 border border-gray-600 rounded mb-2 placeholder-gray-500"
-            placeholder="Parent Name"
-            required
-          />
-          <input
-            type="text"
-            className="w-full p-2 border border-gray-600 rounded mb-2 placeholder-gray-500"
-            placeholder="Phone No"
-            required
-          />
-          <input
-            type="email"
-            className="w-full p-2 border border-gray-600 rounded mb-2 placeholder-gray-500"
-            placeholder="Email"
-            required
-          />
-        </div>
+                {/* The form */}
+                <form
+                    onSubmit={handleFormSubmit}
+                    className="bg-white p-large md:p-xl rounded shadow-lg w-full md:w-1/2 lg:w-[45%] z-20 relative"
+                >
+                    <div className="flex items-center justify-between mb-xl">
+                        <h2 className="uppercase text-primary-maroon-1 text-heading-6 md:text-heading-4">
+                            ENQUIRY FORM
+                        </h2>
 
-        <button
-          type="submit"
-          className="w-[40%] px-4 py-2 hover:bg-white text-black text-subheading uppercase bg-primary-yellow rounded-[24px] mt-4"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+                        <button
+                            onClick={() => onClose(false)}
+                            className="flex items-center justify-center"
+                        >
+                            <CancelIcon
+                                fontSize="large"
+                                style={{ color: "#790000" }}
+                            />
+                        </button>
+                    </div>
+
+                    <fieldset className="mb-large flex flex-col gap-large">
+                        <h4 className="uppercase text-primary-maroon-1 text-subheading md:text-heading-5">
+                            Student Details
+                        </h4>
+                        <input
+                            type="text"
+                            className="w-full p-2 border border-gray-600 rounded placeholder-gray-500"
+                            placeholder="Enter Student Name"
+                            required
+                        />
+                        <input
+                            type="number"
+                            className="w-full p-2 border border-gray-600 rounded placeholder-gray-500"
+                            placeholder="Enter Age"
+                            min="2"
+                            required
+                        />
+                        <input
+                            type="text"
+                            className="w-full p-2 border border-gray-600 rounded placeholder-gray-500"
+                            placeholder="Select Programme"
+                            required
+                        />
+                    </fieldset>
+
+                    <fieldset className="mb-large flex flex-col gap-large">
+                        <h4 className="uppercase text-primary-maroon-1 text-subheading md:text-heading-5">
+                            Parent Details
+                        </h4>
+                        <input
+                            type="text"
+                            className="w-full p-2 border border-gray-600 rounded placeholder-gray-500"
+                            placeholder="Parent Name"
+                            required
+                        />
+                        <input
+                            type="text"
+                            className="w-full p-2 border border-gray-600 rounded placeholder-gray-500"
+                            placeholder="Phone No"
+                            required
+                        />
+                        <input
+                            type="email"
+                            className="w-full p-2 border border-gray-600 rounded placeholder-gray-500"
+                            placeholder="Email"
+                            required
+                        />
+                    </fieldset>
+
+                    <button type="submit" className="btn-slide lg:w-[40%]">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        );
+    }
+
+    function ThankyouContainer() {
+        return (
+            <div className="fixed inset-0 flex justify-center items-center z-50">
+                <div className="section-container bg-white rounded shadow-lg w-[100%] h-[100%] justify-center z-20 flex flex-col">
+                    <div className="flex flex-col gap-large w-full">
+                        <div className="flex items-start justify-between gap-xl">
+                          <h1 className="text-primary-maroon-1 md:text-heading-2 text-heading-6 uppercase">
+                              Thank you for your interest g global school
+                          </h1>
+                          <button
+                              onClick={() => setThankYouOpen(false)}
+                              className="items-center justify-center hidden md:flex"
+                          >
+                              <CancelIcon
+                                  style={{ color: "#790000", fontSize: 48 }}
+                              />
+                          </button>
+                        </div>
+                        <h3 className="uppercase text-primary-maroon-1 text-subheading md:text-heading-5">
+                            our team will contact you soon
+                        </h3>
+                      <a href="" className="btn-slide">
+                        GO BACK
+                      </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    return <>{isThankYouOpen ? <ThankyouContainer /> : <FormContainer />}</>;
 }
--- src/container/Admission/enquiry.tsx
+++ src/container/Admission/enquiry.tsx
@@ -22,12 +22,6 @@ import { useState } from "react";
 import Form from "../../component/Admission-Form/Form";
 const Enquiry = () => {
   const [isFormOpen, setFormOpen] = useState(false);
-  const [isThankYouOpen, setThankYouOpen] = useState(false);
-
-  const handleFormSubmit = () => {
-    setFormOpen(false);
-    setThankYouOpen(true);
-  };
 
   return (
     <section className="section-container">
@@ -57,43 +51,9 @@ const Enquiry = () => {
       </button>
 
       {isFormOpen && (
-        <Form onClose={() => setFormOpen(false)} onSubmit={handleFormSubmit} />
+        <Form onClose={setFormOpen} />
       )}
 
-      {isThankYouOpen && (
-        <div className="fixed inset-0 flex justify-center items-center z-50">
-          {/* Overlay */}
-          <div
-            className="fixed inset-0 bg-black opacity-50 z-10"
-            onClick={() => setThankYouOpen(false)}
-          ></div>
-
-          {/* Thank You Modal */}
-          <div className="bg-white p-8 rounded shadow-lg md:w-[100%] w-[100%] h-[100%] justify-center  z-20 flex flex-col">
-            <div className="flex space-x-xl">
-              <div className="flex flex-col md:w-[85%] w-full">
-                <h1 className="text-primary-maroon-1 md:text-heading-2 text-heading-6 mb-4 uppercase">
-                  Thank you for your interest g global school
-                </h1>
-                <h1 className="uppercase text-primary-maroon-1 md:text-heading-5 text-heading-6">
-                  our team will contact you soon
-                </h1>
-              </div>
-              {/* Close Button */}
-              <button
-                onClick={() => setThankYouOpen(false)}
-                className="  bg-primary-maroon text-white w-16 text-heading-4 h-14 rounded-full transition-all duration-300 "
-              >
-                &times;
-              </button>
-            </div>
-
-            <button className="relative hover:bg-white text-subheading p-md bg-primary-yellow text-black md:w-[35%] w-[70%] rounded-full mt-xl transform transition-transform duration-150 hover:scale-95 hover:shadow-inner ">
-              GO BACK HOME
-            </button>
-          </div>
-        </div>
-      )}
     </section>
   );
 };
