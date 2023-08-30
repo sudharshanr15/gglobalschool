8a6c6ff8aeb2d42b8a42a1fccb9a6d4cae2c5205
Sudharshan <sudharshanrms2004@gmail.com>
2023-08-31 02:03:55 +0530

    Changed hero name

--- src/app/eyp/page.tsx
+++ src/app/eyp/page.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 function page() {
     return (
         <>
-            <Hero title={"Early Year Programme"} />
+            <Hero title={"CURRICULAM AND LEARNING"} />
             <section className="relative section-container bg-primary-maroon-1 ">
                 <div className="absolute top-0 left-0 z-0">
                     <FormatQuoteIcon sx={{ fontSize: 150, opacity: 0.2, color: "#F05A22"}} />
