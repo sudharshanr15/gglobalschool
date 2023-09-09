13cf188d9c976a303e1323bc056a99328d66dfdf
Sudharshan <sudharshanrms2004@gmail.com>
2023-09-09 20:51:19 +0530

    Added swiper on images

--- src/container/life-at-ggs/StudioCards.jsx
+++ src/container/life-at-ggs/StudioCards.jsx
@@ -1,11 +1,33 @@
+'use client';
+
 import React from "react";
 import Image from "next/image";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/effect-fade';
+import { Autoplay, EffectFade } from "swiper/modules";
 
 function StudioCards({ className, img, name, text }) {
   return (
     <section className={className}>
       <div className="w-full md:w-1/2">
-        <Image src={img} alt="logo" className="w-[100%] h-auto" />
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          modules={[EffectFade, Autoplay]}
+          effect="fade"
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+        >
+          {img.map((item, index) => (
+            <SwiperSlide key={index}>
+                <Image src={item} alt="logo" className="w-[100%] h-auto" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
       <div className="w-full md:w-1/2">
         <h2 className="text-heading-4 md:text-heading-3 uppercase lg:w-[90%] text-primary-maroon-1 mb-md">
