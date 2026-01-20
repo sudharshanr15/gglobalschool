f8315db4bd3b519cb9a6f05f7f817ec590262e22
Sudharshan R <sudharshanrms2004@gmail.com>
2026-01-20 23:18:28 +0530

    Replaced admission_open modal image

--- src/component/Modal/Kiddathon.tsx
+++ src/component/Modal/Kiddathon.tsx
@@ -1,40 +1,45 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react'
-import ModalWrapper from './ModalWrapper'
-import admission_open_img from "@/assets/images/modal/admission_open.webp";
-import Image from 'next/image';
+import React, { useEffect, useState } from "react";
+import ModalWrapper from "./ModalWrapper";
+import admission_open_img from "@/assets/images/modal/admission_open.jpg";
+import Image from "next/image";
 
-function Kiddathon(){
-    const [isModalOpen, setisModalOpen] = useState(true);
+function Kiddathon() {
+  const [isModalOpen, setisModalOpen] = useState(true);
 
-    useEffect(() => {
-        if(isModalOpen){
-            document.querySelector("body")!.style.overflow = "hidden"
-        }else{
-            document.querySelector("body")!.style.overflow = "auto"
-        }
-    }, [isModalOpen])
-
-    function onModalClose(){
-        setisModalOpen(false)
+  useEffect(() => {
+    if (isModalOpen) {
+      document.querySelector("body")!.style.overflow = "hidden";
+    } else {
+      document.querySelector("body")!.style.overflow = "auto";
     }
+  }, [isModalOpen]);
 
-    if(!isModalOpen){
-        return <>
-        </>
-    }
+  function onModalClose() {
+    setisModalOpen(false);
+  }
+
+  if (!isModalOpen) {
+    return <></>;
+  }
 
   return (
     <ModalWrapper onModalClose={onModalClose}>
-        <a href="https://forms.gle/sTdV1tgyNgaFExiP8" target='_blank' className="flex flex-col items-center bg-[#E94B37]">
-            <div className="max-h-screen">
-                <Image className='object-contain max-h-[90vh] w-auto' src={admission_open_img} alt='Kiddathon Banner' />
-            </div>
-            <button className='text-white text-sm lg:text-lg underline font-bold py-3'>Register Now</button>
-        </a>
+      {/* <a href="https://forms.gle/sTdV1tgyNgaFExiP8" target='_blank' className="flex flex-col items-center bg-[#E94B37]"> */}
+      <div className="max-h-screen">
+        <Image
+          className="object-contain max-h-[90vh] w-auto"
+          src={admission_open_img}
+          alt="Kiddathon Banner"
+        />
+      </div>
+      {/* <button className="text-white text-sm lg:text-lg underline font-bold py-3">
+        Register Now
+      </button> */}
+      {/* </a> */}
     </ModalWrapper>
-  )
+  );
 }
 
-export default Kiddathon
+export default Kiddathon;
--- src/container/discovery/slide.jsx
+++ src/container/discovery/slide.jsx
@@ -5,71 +5,43 @@ import Image from "next/image";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { Lightbox } from "yet-another-react-lightbox";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/styles.css"
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 
-function Slide({setSlider, gallery_name, gallery_images}) {
+function Slide({ setSlider, gallery_name, gallery_images }) {
+    const thumbnailsRef = useRef(null);
+    const zoomRef = useRef(null);
 
     useEffect(() => {
-        console.log("inside")
-        console.log(gallery_images)
-    }, [])
+        console.log("inside");
+        console.log(gallery_images);
+    }, []);
 
-    const slides=[
-        'Art Competition',
-        'Art Expo',
-        'Christmas',
-        'Independence day',
-        'International Doctors day'
-    ]
-    const swipeslide =gallery_images.map((val, index)=> {
-        return <SwiperSlide className="relative" key={index}>
-            <div className="h-[80vh] w-full">
-                <Image
-                    className="h-full w-full object-cover"
-                    src={val}
-                    alt={gallery_name}
-                    placeholder="blur"
-                />
-            </div>
-            {(index == 0) && (
-                <div className=" bg-primary-maroon-1 absolute bottom-0 m-2xl ">
-                    <p className="text-white md:text-heading-2 text-heading-6 uppercase p-large ">
-                        {gallery_name}
-                    </p>
-                </div>
-            )}
-        </SwiperSlide>
-    })
-
+    const slides = [
+        "Art Competition",
+        "Art Expo",
+        "Christmas",
+        "Independence day",
+        "International Doctors day",
+    ];
     return (
-        <>
-        <div className="fixed top-0 left-0 w-full h-full bg-black opacity-70 flex items-center justify-center" onClick={()=>{setSlider(false)}} id="popup-overlay">
-        </div>
-        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-[80%] w-[90%]">
-            <Swiper
-                style={{
-                    "--swiper-pagination-color": "#790000",
-                    "--swiper-navigation-color": "#790000",
-                    "--swiper-navigation-size": "55px",
-                }}
-                spaceBetween={50}
-                slidesPerView={1}
-                centeredSlides={true}
-                autoplay={{
-                    delay: 1000,
-                    disableOnInteraction: true,
-                }}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                modules={[Autoplay, Pagination, Navigation]}
-            >
-            {swipeslide}
-            </Swiper>
-
-        </div></>
+        <Lightbox
+            open={true}
+            close={() => setSlider(false)}
+            slides={gallery_images}
+            plugins={[Thumbnails, Zoom]}
+            zoom={{ ref: zoomRef }}
+            thumbnails={{ ref: thumbnailsRef }}
+            on={{
+                click: () => {
+                    (thumbnailsRef.current?.visible ? thumbnailsRef.current?.hide : thumbnailsRef.current?.show)?.();
+                },
+            }}
+        />
     );
 }
 export default Slide;
-
